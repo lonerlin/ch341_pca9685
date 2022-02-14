960468ed21f54e0895491829fd9821e4e2938e59
lin <linjungui@126.com>
2022-02-14 11:16:05 +0800

    添加马达实例。

--- example_motor.py
+++ example_motor.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+"""
+使用L298P或者同类的驱动板（两路PWM控制速度，两路数字信号控制方向），
+使用 PCA9685 0~4脚作为输出（PCA9685的0，2脚对应左，右马达的方向，1,3对应左右的PWM）
+"""
 import time
 from pca9685 import PCA9685
 i2c = PCA9685(0x40, debug=True)  # 对象初始化，指定IIC地址
